b8ec82f0c6f3de1d1b1055563e001aab663dbfe7
FooTage <pookair@gmail.com>
2018-05-23 23:40:53 +0300

    sphere intersection

--- src/edu/cg/scene/objects/Sphere.java
+++ src/edu/cg/scene/objects/Sphere.java
@@ -1,9 +1,7 @@
 package edu.cg.scene.objects;
 
 import edu.cg.UnimplementedMethodException;
-import edu.cg.algebra.Hit;
-import edu.cg.algebra.Point;
-import edu.cg.algebra.Ray;
+import edu.cg.algebra.*;
 
 public class Sphere extends Shape {
 	private Point center;
@@ -38,7 +36,35 @@ public class Sphere extends Shape {
 	
 	@Override
 	public Hit intersect(Ray ray) {
-		//TODO: implement this method.
-		throw new UnimplementedMethodException("intersect(Ray)");
+		Vec L = center.sub(ray.source());
+		double tm = L.dot(ray.direction());
+		double d2 = L.lengthSqr() - (tm * tm);
+		double r2 = Math.pow(radius, 2);
+		if(d2 > r2){
+			return null;
+		}
+		double th = Math.sqrt(r2 - d2);
+		double t1 = tm - th;
+		double t2 = tm + th;
+
+		if (t2 < Ops.epsilon){
+            return null;
+        }
+        if (t1 < Ops.epsilon){
+            if (t2 > Ops.infinity){
+                return null;
+            }
+            return new Hit(t2, getNormalAtPoint(ray.add(t2)).neg());
+        }
+
+        if (t1 > Ops.infinity){
+		    return null;
+        }
+
+        return new Hit(t1, getNormalAtPoint(ray.add(t1)));
+	}
+
+	private Vec getNormalAtPoint(Point p) {
+		return p.sub(this.center).normalize();
 	}
 }
